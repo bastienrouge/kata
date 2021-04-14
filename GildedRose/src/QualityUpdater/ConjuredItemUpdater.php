ee83de8878febb8d3098efe50c6e66d00b48003d
Bastien Rougé <bastien.rouge@new-immo-group.com>
2021-04-14 19:48:31 +0200

    Edit ConjuredItem and update unit tests

--- GildedRose/src/QualityUpdater/ConjuredItemUpdater.php
+++ GildedRose/src/QualityUpdater/ConjuredItemUpdater.php
@@ -8,8 +8,8 @@ class ConjuredItemUpdater extends ItemUpdaterAbstract
 {
     public function updateQuality(): void
     {
-        if ($this->getItem()->quality > 0) {
-            $this->getItem()->quality--;
+        if ($this->getItem()->quality > 1) {
+            $this->getItem()->quality -= 2;
         }
 
         if ($this->getItem()->quality > 0 && $this->getItem()->sellIn < 1) {
